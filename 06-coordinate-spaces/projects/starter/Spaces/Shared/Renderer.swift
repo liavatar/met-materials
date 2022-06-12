589d8301a0f35aaafec7b09a72d32ad91e0d8562
liavatar <liavatar@gmail.com>
2022-06-12 01:45:21 -0700

    adding chapter 6

--- 06-coordinate-spaces/projects/starter/Spaces/Shared/Renderer.swift
+++ 06-coordinate-spaces/projects/starter/Spaces/Shared/Renderer.swift
@@ -1,34 +1,4 @@
-/// Copyright (c) 2022 Razeware LLC
-///
-/// Permission is hereby granted, free of charge, to any person obtaining a copy
-/// of this software and associated documentation files (the "Software"), to deal
-/// in the Software without restriction, including without limitation the rights
-/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-/// copies of the Software, and to permit persons to whom the Software is
-/// furnished to do so, subject to the following conditions:
-///
-/// The above copyright notice and this permission notice shall be included in
-/// all copies or substantial portions of the Software.
-///
-/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
-/// distribute, sublicense, create a derivative work, and/or sell copies of the
-/// Software in any work that is designed, intended, or marketed for pedagogical or
-/// instructional purposes related to programming, coding, application development,
-/// or information technology.  Permission for such use, copying, modification,
-/// merger, publication, distribution, sublicensing, creation of derivative works,
-/// or sale is expressly withheld.
-///
-/// This project and source code may use libraries or frameworks that are
-/// released under various Open-Source licenses. Use of those libraries and
-/// frameworks are governed by their own individual licenses.
-///
-/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-/// THE SOFTWARE.
+
 
 import MetalKit
 
@@ -45,6 +15,8 @@ class Renderer: NSObject {
   }()
 
   var timer: Float = 0
+  
+  var uniforms = Uniforms()
 
   init(metalView: MTKView) {
     guard
@@ -86,14 +58,24 @@ class Renderer: NSObject {
       blue: 0.9,
       alpha: 1.0)
     metalView.delegate = self
+    
+    uniforms.viewMatrix = float4x4(translation: [-0.8, 0, 0])
+
+    mtkView(metalView, drawableSizeWillChange: metalView.bounds.size)
   }
 }
 
 extension Renderer: MTKViewDelegate {
-  func mtkView(
-    _ view: MTKView,
-    drawableSizeWillChange size: CGSize
-  ) { }
+  func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
+    let aspect = Float(view.bounds.width) / Float(view.bounds.height)
+    let projectionMatrix =
+      float4x4(
+        projectionFov: Float(70).degreesToRadians,
+        near: 0.1,
+        far: 100,
+        aspect: aspect)
+    uniforms.projectionMatrix = projectionMatrix
+  }
 
   func draw(in view: MTKView) {
     guard
@@ -107,6 +89,18 @@ extension Renderer: MTKViewDelegate {
 
     renderEncoder.setRenderPipelineState(pipelineState)
     renderEncoder.setTriangleFillMode(.lines)
+
+    timer += 0.005
+    uniforms.viewMatrix = float4x4(translation: [0, 0, 3])
+    
+    model.position.y = -0.6
+    model.rotation.y = sin(timer)
+    uniforms.modelMatrix = model.transform.modelMatrix
+
+    renderEncoder.setVertexBytes(
+      &uniforms,
+      length: MemoryLayout<Uniforms>.stride,
+      index: 11)
 
     model.render(encoder: renderEncoder)
 
